a01a9c21fd7d64c3940723e46b1af79c8d580c0c
indigogog <hamer000711@gmail.com>
2021-11-23 16:30:32 +0300

    second commit

---
+++ src/stores/counterStoreClass.js
@@ -0,0 +1,34 @@
+import { action, makeObservable ,observable, computed, makeAutoObservable, autorun, when,reaction } from 'mobx';
+
+export class counterStoreClass{
+    count = 0; 
+
+    //computed
+    get color(){
+        return this.count > 0 ? 'green' : this.count <0 ? 'red' : 'black';
+    }
+
+    constructor() {
+        // makeObservable(this, {
+        //     count: observable,
+        //     color: computed,
+        //     dec: action,
+        //     inc: action.bound,            
+        // });
+        makeAutoObservable(this, {
+            inc: action.bound,
+        });
+
+       // autorun(()=> console.log('Count:', this.count));
+        when(()=> this.count>5, ()=> console.log('ccc'));
+        reaction(()=> this.count, (count, prevCount) =>{
+            console.log('prevCount: ', prevCount,'count: ', count);
+        })
+    }
+
+    dec = () => this.count--;
+
+    inc(){
+        this.count++;
+    }
+}
---
+++ src/stores/index.js
@@ -0,0 +1,2 @@
+export * from './counterStoreClass.js'
+export * from './counterStoreF.js'
